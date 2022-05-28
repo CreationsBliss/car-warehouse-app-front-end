6982d09aae861a0a67d3cc307bcf62824bc5f66c
Musfakur <procreationsbliss@gmail.com>
2022-05-28 08:21:05 +0600

    load single item detail

--- src/Pages/InventoryItemDetail/InventoryItemDetail.js
+++ src/Pages/InventoryItemDetail/InventoryItemDetail.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './InventoryItemDetail.css';
 
 const InventoryItemDetail = () => {
 
-const {id} = useParams();
+  const { id } = useParams();
+
+  const [inventory, setInventory] = useState({});
+
+
+  useEffect(() => {
+    const url = `http://localhost:5000/inventory/${id}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setInventory(data));
+  }, [])
 
   return (
-    <div>
-      <h2> Welcome to Item Detail {id} </h2>
-    </div>
+    <div className='inventory-item-detail-container'>
+    <img src={inventory.image} alt="car pic" />
+    <h4>{inventory.name} </h4>
+    <h6 className='py-3'>Product Id: {inventory._id} </h6>
+    <p>Price: {inventory.price}</p>
+    <p>Product Quantity: {inventory.productQuantity}</p>
+    <p>Supplier Name: {inventory.supplierName}</p>
+    <p> <small>{inventory.description}</small> </p>
+    <button className='update-stock-btn'> Delivered </button>
+  </div>
   );
 };
 
